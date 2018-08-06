5382e6949cdfa4aec23a0faa0e46e3a00fc15520
Hadi <hadi.tabatabaee@lge.com>
2018-08-06 11:31:03 -0700

    Slight modifications to dev_stop script.

--- docker/scripts/dev_stop.sh
+++ docker/scripts/dev_stop.sh
@@ -2,10 +2,12 @@
 
 # Script to (somewhat) gracefully stop all apollo related containers.
 
-docker exec apollo_dev /apollo/scripts/bootstrap.sh stop
+docker exec -d apollo_dev /apollo/scripts/bootstrap.sh stop
 
 docker stop apollo_dev
 docker stop apollo_yolo3d_volume
 docker stop apollo_localization_volume
 docker stop apollo_map_volume-sunnyvale_loop
 docker stop apollo_map_volume-sunnyvale_big_loop
+
+echo "Apollo docker containers have been stopped."
