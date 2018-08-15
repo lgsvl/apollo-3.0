ac1fdacde269c0d2a3c985521b74adceaf9a1dc4
Hadi <hadi.tabatabaee@lge.com>
2018-08-15 16:08:52 -0700

    Changed docker stop to docker kill

--- docker/scripts/dev_stop.sh
+++ docker/scripts/dev_stop.sh
@@ -2,12 +2,10 @@
 
 # Script to (somewhat) gracefully stop all apollo related containers.
 
-docker exec -d apollo_dev /apollo/scripts/bootstrap.sh stop
+docker kill apollo_dev
+docker kill apollo_yolo3d_volume
+docker kill apollo_localization_volume
+docker kill apollo_map_volume-sunnyvale_loop
+docker kill apollo_map_volume-sunnyvale_big_loop
 
-docker stop apollo_dev
-docker stop apollo_yolo3d_volume
-docker stop apollo_localization_volume
-docker stop apollo_map_volume-sunnyvale_loop
-docker stop apollo_map_volume-sunnyvale_big_loop
-
-echo "Apollo docker containers have been stopped."
+echo "Apollo docker containers have been killed."
