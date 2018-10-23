a49a286269c13cff85f9da56fee74bcb31990f77
Hadi <hadi.tabatabaee@lge.com>
2018-10-22 18:50:17 -0700

    Allowing dev_start to images with load custom tags

--- docker/scripts/dev_start.sh
+++ docker/scripts/dev_start.sh
@@ -21,7 +21,7 @@ INCHINA="no"
 LOCAL_IMAGE="no"
 VERSION=""
 ARCH=$(uname -m)
-VERSION_X86_64="dev-x86_64-20180906_2002"
+VERSION_X86_64="latest"
 VERSION_AARCH64="dev-aarch64-20170927_1111"
 VERSION_OPT=""
 
@@ -171,7 +171,7 @@ if [ "$LOCAL_IMAGE" == "yes" ] && [ -z "$VERSION_OPT" ]; then
 fi
 
 
-IMG=lgsvl/apollo:latest
+IMG=lgsvl/apollo:${VERSION}
 
 function local_volumes() {
     # Apollo root and bazel cache dirs are required.
@@ -225,7 +225,7 @@ function main(){
         display="${DISPLAY}"
     fi
 
-    setup_deviceDOCKER_CMD
+    setup_device
 
     USER_ID=$(id -u)
     GRP=apollo
